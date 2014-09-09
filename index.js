e252ef402e6a88ba061145b89265311aa94596c0
Nathan Bowser <nathan.bowser@spiderstrategies.com>
2014-09-09 10:25:03 -0400

    builds a server

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-var historical = require('./lib/historical')
-  , current = require('./lib/current')
-  , ticker = process.argv[2]
+var http = require('http')
+  , url = require('url')
+  , fetch = require('./lib')
 
-if (!ticker) {
-  throw new Error('Define a ticker')
-}
-historical(ticker, function (err, result) {
-  current(ticker, function (err, value) {
-    result.value = value
-    console.log(result)
+http.createServer(function (req, res) {
+  var ticker = url.parse(req.url, true).query.ticker
+  if (!ticker) {
+    res.writeHead(500)
+    res.end('Missing ticker')
+  }
+  fetch(ticker, function (err, result) {
+    if (err) {
+      res.writeHead(500)
+      return res.end(err.message)
+    }
+    res.writeHead(200, {'Content-Type': 'application/json'})
+    res.end(JSON.stringify(result))
   })
-})
+}).listen(process.env.PORT || 9000, 'localhost')
