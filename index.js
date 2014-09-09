c40c7687922dad137ca9b9959eabaf02d40af621
Nathan Bowser <nathan.bowser@spiderstrategies.com>
2014-09-09 10:10:39 -0400

    fetch historical start ends

---
+++ index.js
@@ -0,0 +1,27 @@
+var request = require('request')
+  , split = require('split')
+  , reduce = require('stream-reduce')
+  , yahoooooooooooo = 'http://ichart.yahoo.com/table.csv'
+
+var from = new Date
+  , result = {
+    start: Infinity,
+    end: -Infinity
+  }
+
+from.setMonth(from.getMonth() - 1)
+
+request(yahoooooooooooo + '?s=AAPL&a=' + from.getMonth() + '&b=' + from.getDate() + '&c=' + from.getFullYear())
+  .pipe(split())
+  .pipe(reduce(function (acc, data) {
+    var row = data.split(',').map(Number)
+    if (row[2]) {
+      result.end = Math.max(result.end, row[2])
+    }
+    if (row[3]) {
+      result.start = Math.min(result.start, row[3])
+    }
+    return result
+  }, result)).on('data', function (d) {
+    console.log(d)
+  })
